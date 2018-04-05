eeeb41c23ef83a2d4b597d6372b89b8e6c7c0642
Nicholas Shirley <nicholas@reallymy.email>
2018-04-05 10:07:55 +0200

    scope to user_id

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe 'validations' do
-    it { should validate_uniqueness_of(:name).case_insensitive.scoped_to(:user) }
+    it { should validate_uniqueness_of(:name).case_insensitive.scoped_to(:user_id) }
   end
 end
